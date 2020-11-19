a54da6b198609f37017a70e7e656b51daff25477
Emily Flynn <Emily.Flynn@softwire.com>
2020-11-19 15:47:12 +0000

    Implement simple error logging

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,14 +1,20 @@
 package training.supportbank;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.List;
 
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public static void main(String[] args) {
         CSVParser csvParser = new CSVParser("Transactions2014.csv");
         List<String[]> transactionTable = csvParser.getTable();
         if (transactionTable == null)
         {
             System.out.println("Error reading file! Please ensure file exists.");
+            LOGGER.error("Error reading file");
             return;
         }
         AccountsRegister accountsRegister = new AccountsRegister(transactionTable);
