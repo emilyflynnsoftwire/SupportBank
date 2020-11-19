bd7bd6f547b3427836382a68d91095509ce2ac45
Joseph Silcock <joseph.silcock@softwire.com>
2020-11-19 15:39:01 +0000

    Added command line interaction

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        CSVParser csvParser = new CSVParser("DodgyTransactions2015.csv");
+        CSVParser csvParser = new CSVParser("Transactions2014.csv");
         List<String[]> transactionTable = csvParser.getTable();
         if (transactionTable == null)
         {
@@ -13,8 +13,13 @@ public class Main {
         }
         AccountsRegister accountsRegister = new AccountsRegister(transactionTable);
 
-        accountsRegister.printAllAccountsAndBalances();
-        System.out.println();
-        accountsRegister.printTransactionsForPerson("Sarah T");
+        if(args.length!=0) {
+            if (args[1].toLowerCase().equals("all")) {
+                accountsRegister.printAllAccountsAndBalances();
+            } else{
+                String personToList = args[1] + " " + args[2];
+                accountsRegister.printTransactionsForPerson(personToList);
+            }
+        }
     }
 }
